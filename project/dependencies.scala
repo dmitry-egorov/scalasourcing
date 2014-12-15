420ab32c445f51de9ce7c60b5802ac6e58d8dc07
Dmitry Egorov <degorov86@gmail.com>
2014-12-15 22:54:13 +0300

    removed dependency off BDD module on ScalaTest

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -5,6 +5,6 @@ object Dependencies
     val scalatest = "org.scalatest" % "scalatest_2.11" % "2.2.1"
 
     val coreDependencies: Seq[ModuleID] = Seq()
-    val bddDependencies: Seq[ModuleID] = Seq(scalatest)
+    val bddDependencies: Seq[ModuleID] = Seq()
     val exampleDependencies: Seq[ModuleID] = Seq(scalatest)
 }
